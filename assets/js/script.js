9c7c6e51f7d35726a1a22319247606b578ec41a0
Luís Mendes <soconfirmo@hotmail.com>
2024-08-27 21:11:37 -0300

    Add support for multiples marketplaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 
 // Prevent context menu
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+function openModal() {
+    document.getElementById('marketplacesModal').classList.remove('hidden');
+}
+
+function closeModal() {
+    document.getElementById('marketplacesModal').classList.add('hidden');
+}
+
+document.querySelector('#marketplaces').addEventListener('click', function(event) {
+    event.preventDefault();
+    openModal();
+});
